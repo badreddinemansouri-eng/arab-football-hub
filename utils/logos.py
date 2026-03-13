2c4e807ae69f9d5b6656c7c6a2a09c23d964f52f
badreddinemansouri-eng <badreddine.mansouri@etudiant-fst.utm.tn>
2026-03-13 20:23:35 +0100

    Update logos.py

--- utils/logos.py
+++ utils/logos.py
@@ -4,7 +4,7 @@ import streamlit as st
 import hashlib
 import unicodedata
 import re
-
+import functools
 supabase = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_ANON_KEY"])
 
 def normalize_name(name):
